e7abe3dbabc223a2d931f00443b4af2eab943e42
Lars Luijendijk <larsluijendijk@gmail.com>
2016-11-01 13:34:43 +0100

    div

--- views/welcome.blade.php
+++ views/welcome.blade.php
@@ -4,15 +4,22 @@
 <link href="css/style.css" rel="stylesheet" type="text/css">
 <title>{{$title}}</title>
 </head>
+<body>
+<div class="topmenu">
+<ul>
+	<li><a class="active" href="welcome.php">Home</a></li>
+	<li><a href="events.php">Events</a></li>	
+	<li><a href="contact.php">Contact</a></li>
+</ul>
+</div>
 <div class="title">
 <h2>Welcome to my site!</h1>
 </div>
 <div class="container">
 <p>hey</p>
-
-
-
 </div>
-<body>
+
 </body>
+</div>
+
 </html>
